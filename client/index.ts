17233df03360fd152fc1cd773114794f92a56859
Shigma <shigma10826@gmail.com>
2022-12-18 17:31:50 +0800

    feat: initialize client

---
+++ client/index.ts
@@ -0,0 +1,18 @@
+import { Context, icons } from '@koishijs/client'
+import type {} from 'koishi-plugin-migration/src'
+import Migration from './migration.vue'
+import Activity from './icons/activity.vue'
+
+icons.register('migration', Activity)
+
+export default (ctx: Context) => {
+  ctx.page({
+    path: '/migration',
+    name: '更新与迁移',
+    icon: 'migration',
+    order: 650,
+    authority: 4,
+    fields: ['dependencies'],
+    component: Migration,
+  })
+}
---
+++ client/migration.vue
@@ -0,0 +1,23 @@
+<template>
+  <k-layout main="darker" class="page-migration">
+    <k-card>
+      您当前的 Koishi 版本为 {{ store.dependencies.koishi.resolved }}，无需升级。
+    </k-card>
+  </k-layout>
+</template>
+
+<script lang="ts" setup>
+
+import { store } from '@koishijs/client'
+
+</script>
+
+<style lang="scss">
+
+.page-migration {
+  .k-card {
+    margin: 1rem;
+  }
+}
+
+</style>
